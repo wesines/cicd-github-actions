598d08d8938b3deb41abd7482458713c99bd5f7e
wesines <ines.oueslati.attia@gmail.com>
2024-02-04 12:43:13 +0100

    init project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Le bon developpeur :) ')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
